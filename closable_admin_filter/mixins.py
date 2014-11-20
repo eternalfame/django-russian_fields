9bbb88892f7e5ad10c96b105c3a2eae3df473ecf
Vassal Sunchoke <eternalfame@mail.ru>
2014-11-20 13:37:20 +1000

    Have forgotten to include the CSS

--- closable_admin_filter/mixins.py
+++ closable_admin_filter/mixins.py
@@ -4,6 +4,11 @@ from django.contrib.admin import ModelAdmin
 
 class AdminClosableFilterMixin(ModelAdmin):
     class Media:
-        js = [
+        css = {
+            'screen': (
+                settings.STATIC_URL + '/admin/css/closable_admin_filter.css',
+            )
+        }
+        js = (
             settings.STATIC_URL + 'admin/js/closable_admin_filter.js',
-        ]
+        )
