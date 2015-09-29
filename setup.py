ba7b9386ea332f619f67909b7c3e1ebacdb417f3
eternalfame <eternalfame@mail.ru>
2015-09-29 11:00:47 +1000

    changed version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-russian_fields',
-    version='0.1.0',
+    version='0.1.1',
     packages=['russian_fields'],
     include_package_data=True,
     license='MIT License',
